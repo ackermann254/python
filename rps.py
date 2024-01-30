8203209e06df1a80d52f9890686bb1f794f79ec5
ackermann254 <brianototo1@gmail.com>
2024-01-30 18:54:59 +0300

    these are the responsive changes i made with loop to make the game repeat with ease  when a  gamer games

--- rps.py
+++ rps.py
@@ -7,31 +7,48 @@ class RPS(Enum):
     PAPER = 2
     SCISSORS = 3
 
+playagain = True
 
-print("")
-playerchoice = input("Enter...\n1 for Rock,\n2 for Paper,\n3 for scissors:\n\n")
+while playagain:
 
-player = int(playerchoice)
 
-if player < 1  or player > 3: 
-    sys.exit("You must enter 1, 2, or 3.")
+  
+    playerchoice = input("\nEnter...\n1 for Rock,\n2 for Paper,\n3 for scissors:\n\n")
 
-computerchoice = random.choice("123")
+    player = int(playerchoice)
 
-computer = int(computerchoice)
+    if player < 1  or player > 3: 
+        sys.exit("You must enter 1, 2, or 3.")
 
-print("")
-print("You chose " + str(RPS(player)).replace('RPS.','' ) +".")
-print("python chose" + str(RPS(computer)).replace('RPS.','' ) +".")
-print("")
+    computerchoice = random.choice("123")
 
-if player == 1 and computer == 3:
-    print("you win!")
-elif player == 2 and computer == 1:
-    print("you win!")
-elif player == 3 and computer == 2:
-    print("you win!")
-elif player == computer:
-    print("Tie game!")
-else:
-    print("python wins!")    
+    computer = int(computerchoice)
+
+   
+    print("\nYou chose " + str(RPS(player)).replace('RPS.','' ) +".")
+    print("python chose" + str(RPS(computer)).replace('RPS.','' ) +".\n")
+    
+
+    if player == 1 and computer == 3:
+        print("you win!")
+    elif player == 2 and computer == 1:
+        print("you win!")
+    elif player == 3 and computer == 2:
+        print("you win!")
+    elif player == computer:
+        print("Tie game!")
+    else:
+        print("python wins!")    
+
+    playagain = input("\nPlay again? \nY for Yes or \nQ to quit\n\n")  
+    if playagain.lower() == "y":
+        continue
+    else:
+        print("\ncelebration") 
+
+        print("Thank you for playing again!\n")
+
+        playagain = False
+
+
+sys.exit ("Bye!!!!!!!!!!!")    
